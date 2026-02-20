b7d10a220af58a066b4316b9b24272640137f7fc
zhangutah <yun.zhang@utah.edu>
2026-02-20 16:15:26 -0700

    update cpp targets

--- agent/run_gen.py
+++ agent/run_gen.py
@@ -195,8 +195,9 @@ if __name__ == "__main__":
         # "/home/yk/code/LLM-reasoning-agents/cfg/gpt5_mini/c_study/gpt5_mini_basic+header+example.yaml",
         # "/home/yk/code/LLM-reasoning-agents/cfg/gpt5_mini/c_study/gpt5_mini_basic+header+definition.yaml",
         # "/home/yk/code/LLM-reasoning-agents/cfg/gpt5_mini/c_study/gpt5_mini_basic+header+issta.yaml",
-        # "/home/yk/code/LLM-reasoning-agents/cfg/gpt5_mini/c_study/gpt5_mini_basic+header+ossfuzz.yaml"
-        "/home/yk/code/LLM-reasoning-agents/cfg/gpt5_mini/projects/gpt5_mini_agent_mosquitto.yaml"
+        # "/home/yk/code/LLM-reasoning-agents/cfg/gpt5_mini/c_study/gpt5_mini_basic+header+ossfuzz.yaml",
+        # "/home/yk/code/LLM-reasoning-agents/cfg/gpt5_mini/projects/gpt5_mini_agent_mosquitto.yaml",
+        "/mydata/data/fuzzing/harnessgen/LLM-reasoning-agents/cfg/gpt5_mini/gpt5_mini_agent_cpp.yaml"
     ]
     for config_path in cfg_list:
         runner = Runner(config_path)
